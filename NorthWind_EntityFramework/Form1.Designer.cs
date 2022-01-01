593a88ec6a73eecf63c5b9034835263b820443b9
ohad2 <ohad2@LAPTOP>
2022-01-01 17:12:31 +0200

    Add project files.

---
+++ NorthWind_EntityFramework/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace NorthWind_EntityFramework
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShow = new System.Windows.Forms.Button();
+            this.comBoxQues = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblLoading = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(292, 44);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(100, 31);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // comBoxQues
+            // 
+            this.comBoxQues.FormattingEnabled = true;
+            this.comBoxQues.Items.AddRange(new object[] {
+            "Question 1",
+            "Question 2",
+            "Question 3",
+            "Question 4",
+            "Question 5",
+            "Question 6",
+            "Question 7",
+            "Question 8",
+            "Question 9",
+            "Question 10"});
+            this.comBoxQues.Location = new System.Drawing.Point(41, 44);
+            this.comBoxQues.Name = "comBoxQues";
+            this.comBoxQues.Size = new System.Drawing.Size(205, 23);
+            this.comBoxQues.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 101);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 337);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // lblLoading
+            // 
+            this.lblLoading.AutoSize = true;
+            this.lblLoading.Location = new System.Drawing.Point(437, 59);
+            this.lblLoading.Name = "lblLoading";
+            this.lblLoading.Size = new System.Drawing.Size(59, 15);
+            this.lblLoading.TabIndex = 3;
+            this.lblLoading.Text = "Loading...";
+            this.lblLoading.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblLoading);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comBoxQues);
+            this.Controls.Add(this.btnShow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.ComboBox comBoxQues;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblLoading;
+    }
+}
+
---
+++ NorthWind_EntityFramework/Form1.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthWind_EntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NorthWind_EntityFramework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnShow_Click(object sender, EventArgs e)
+        {
+            lblLoading.Visible = true;
+            using (var db = new NorthwindContext())
+            {
+                switch (comBoxQues.Text)
+                {
+                    case "Question 1":
+                        var query1 = db.Products.Select(p => new { p.ProductName, p.QuantityPerUnit });
+                        dataGridView1.DataSource = await Task.Run(() => query1.ToList());
+                        lblLoading.Visible = false;
+                        break;
+                    case "Question 2":
+                        var query2 = db.Products.Where(p => p.Discontinued == false).Select(p => new {p.ProductId , p.ProductName});
+                        dataGridView1.DataSource = await Task.Run(() => query2.ToList());
+                        lblLoading.Visible = false;
+                        break;
+                    case "Question 3":
+                        var query3 = db.Products.Where(p => p.Discontinued == true).Select(p => new { p.ProductId, p.ProductName });
+                        dataGridView1.DataSource = await Task.Run(() => query3.ToList());
+                        lblLoading.Visible = false;
+                        break;
+                    case "Question 4":
+                        var query4 = db.Products.Where(p => p.UnitPrice == db.Products.Max(p => p.UnitPrice) || p.UnitPrice == db.Products.Min(p => p.UnitPrice))
+                            .Select(p => new { p.ProductName, p.UnitPrice });
+                        dataGridView1.DataSource = await Task.Run(() => query4.ToList());
+                        lblLoading.Visible = false;
+                        break;
+                    case "Question 5":
+                        var query5 = db.Products.Where(p => p.UnitPrice < 20)
+                            .Select(p => new { p.ProductId, p.ProductName, p.UnitPrice });
+                        dataGridView1.DataSource = await Task.Run(() => query5.ToList());
+                        lblLoading.Visible = false;
+                        break;
+                    case "Question 6":
+                        var query6 = db.Products.Where(p => p.UnitPrice <= 25 && p.UnitPrice >= 15)
+                            .Select(p => new { p.ProductId, p.ProductName, p.UnitPrice });
+                        dataGridView1.DataSource = await Task.Run(() => query6.ToList());
+                        lblLoading.Visible = false;
+                        break;
+                    case "Question 7":
+                        var query7 = db.Products.Where(p => p.UnitPrice > db.Products.Average(p => p.UnitPrice))
+                            .Select(p => new { p.ProductName, p.UnitPrice });
+                        dataGridView1.DataSource = await Task.Run(() => query7.ToList());
+                        lblLoading.Visible = false;
+                        break;
+                    case "Question 8":
+                        var query8 = db.Products.Where(p => 10 >= (db.Products.Where(q => q.UnitPrice >= p.UnitPrice).Distinct().Count()) )
+                            .Select(p => new { p.ProductName, p.UnitPrice }).OrderByDescending(p => p.UnitPrice);
+                        dataGridView1.DataSource = await Task.Run(() => query8.ToList());
+                        lblLoading.Visible = false;
+                        break;
+                    case "Question 9":
+                        var query9 = from p in db.Products
+                                     group p by p.Discontinued into pl
+                                     select new { Discontinued = pl.Key, Count = pl.Count() };
+                                                  
+                        dataGridView1.DataSource = await Task.Run(() => query9.ToList());
+                        lblLoading.Visible = false;
+                        break;
+                    case "Question 10":
+                        var query10 = from p in db.Products
+                                     where p.UnitsInStock < p.UnitsOnOrder
+                                     select new {p.ProductName , p.UnitsOnOrder , p.UnitsInStock};
+
+                        var lambdaQuery10 = db.Products.Where(p => p.UnitsInStock < p.UnitsOnOrder)
+                            .Select(p => new { p.ProductName, p.UnitsOnOrder, p.UnitsInStock });
+
+                        dataGridView1.DataSource = await Task.Run(() => lambdaQuery10.ToList());
+                        lblLoading.Visible = false;
+                        break;
+
+                }
+            }
+        }
+    }
+}
